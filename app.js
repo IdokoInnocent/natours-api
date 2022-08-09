99842ea5dd30c20e2f3cc1687b3a322dd7908d42
Idoko Innocent <iidoko899@gmail.com>
2022-08-09 17:42:15 +0100

    Added comment to the app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
-// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
