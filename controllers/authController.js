058c15caa7aca4c58a79b51915c91abb546c5c25
Idoko Innocent <iidoko899@gmail.com>
2022-08-10 01:51:24 +0100

    Added heroku config

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,17 +12,15 @@ const signToken = id => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
-  };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+  });
 
   //  Remove password from output
   user.password = undefined;
@@ -48,7 +46,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
 
   await new Email(newUser, url).sendWelcome();
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -78,7 +76,7 @@ exports.login = catchAsync(async (req, res, next) => {
   //   status: 'success',
   //   token
   // });
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -247,7 +245,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //   status: 'success',
   //   token
   // });
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 // Update password. This is for users who just want to change their password when they are already logged in.
@@ -273,5 +271,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   // });
 
   // 4) Log in user, send JWT
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
