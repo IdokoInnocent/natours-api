b13279b70561da0314f42dd700c451a841e307c5
Idoko Innocent <iidoko899@gmail.com>
2022-08-26 01:56:27 +0100

    Modified app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'development') {
 // Limit request from same API
 // Thi middleware is for security purpose. That is it blocks request coming from aparticular id if it is too much
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in one hour!'
 });
